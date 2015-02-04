005ad804442674f4da84453dc557ad64ec6751e6
Tyler Lubeck <tyler@tylerlubeck.com>
2015-02-03 22:51:12 -0500

    Register models with the admin

--- marauder/admin.py
+++ marauder/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Floor, Location, AccessPoint
+
+admin.site.register(Floor)
+
+admin.site.register(Location)
+
+admin.site.register(AccessPoint)
