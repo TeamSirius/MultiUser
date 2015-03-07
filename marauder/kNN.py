ff6c51957180e10371462fbaa3786d1d19f2a796
bfisch02 <brett.fischler@tufts.edu>
2015-03-07 15:29:25 -0500

    Updated kNN function to work with django

---
+++ marauder/kNN.py
@@ -0,0 +1,217 @@
+# Brett (Berty) Fischler and Hunter (Kenneth) Wapman
+# October 2014
+# kNN Implementation for Senior Design Project
+
+from collections import Counter
+import sets
+import math
+import sys
+import os
+from math import isinf
+
+# Minimum normalized RSSI value detected; used as "not detected" value
+MIN_DETECTED = 0
+
+
+# Access Point class
+class AccessPoint(object):
+    def __init__(self, ap, from_django=False):
+        if not from_django:
+            self.mac = ap[0]
+            self.strength_dbm = float(ap[1])
+            self.strength = 10 ** (float(ap[1]) / 10)
+            self.std = 10 ** (float(ap[2]) / 10)
+            self.datetime = ap[3]
+            #self.sample_size = ap[4]
+        else:
+            self.mac = ap['mac_address']
+            self.strength_dbm = ap['signal_strength']
+            self.strength = 10 ** (self.strength_dbm / 10)
+            self.std = 10 ** (ap['standard_deviation'] / 10)
+            self.datetime = ap['recorded']
+
+# Location Class
+# TODO: Look into storing previous distance calculations
+class Location(object):
+    def __init__(self, loc):
+        self.x = loc[0]
+        self.y = loc[1]
+        self.direction = loc[2]
+        self.floor_id = loc[3]
+        self.init_aps(loc[4])
+
+    def printLoc(self):
+        sys.stdout.write("Location: (x, y) = (" + str(self.x) + ", " + str(self.y) + \
+                "), Floor = " + str(self.floor_id) + "\n")
+
+    # Stores Access Points in a {mac_id : AccessPoint} dictionary
+    def init_aps(self, aps):
+        self.aps = {}
+        for ap in aps:
+            self.aps[ap[0]] = AccessPoint(ap)
+
+    #Calculates distance between this Location and the given dictionary of
+    #AccessPoints (currently calls function to calculate Euclidean distance)
+    def get_distance1(self, aps):
+        distances = []
+        keys = sets.Set()
+        for mac_id in aps.keys():
+            keys.add(mac_id)
+        for mac_id in self.aps.keys():
+            keys.add(mac_id)
+        euc_dist = euclidean(keys, self.aps, aps)
+        percent_shared = float(len([ap for ap in aps.keys() if ap in self.aps.keys()])) / len(keys)
+        if percent_shared == 0:
+            return float("INF")
+        return 1 / percent_shared + 1.5 * euc_dist
+
+    def get_distance2(self, aps):
+        num_similar = 0
+        for mac_id in aps.keys():
+            if mac_id in self.aps.keys():
+                num_similar += 1
+        return num_similar
+
+# Given a set of mac_ids and two dictionaries of AccessPoints, calculates the
+# Euclidean distance between the two dictionaries
+def euclidean(keys, aps1, aps2):
+    rVal = 0
+    for key in keys:
+        strength1 = MIN_DETECTED
+        if key in aps1:
+            strength1 = aps1[key].strength
+        strength1 = 10 ** (strength1 / 10)
+        strength2 = MIN_DETECTED
+        if key in aps2:
+            strength2 = aps2[key].strength
+        strength2 = 10 ** (strength2 / 10)
+        rVal = rVal + (strength1 - strength2) ** 2
+    return math.sqrt(rVal)
+
+
+        # Given a list of tuples where t[0] is the value and t[1] is the distance,
+# returns a weighted average of the values
+def weighted_avg(tuples, inverse):
+    ### If we want the unweighted average:
+    #return sum([t[0] for t in tuples]) / len(tuples)
+    s = 0
+    for t in tuples:
+        if t[1] == 0:
+            return t[0]
+    if inverse:
+        weight_sum = sum([1 / t[1] for t in tuples])
+    else:
+        weight_sum = sum([t[1] for t in tuples])
+    for t in tuples:
+        if isinf(t[1]) or weight_sum == 0:
+            print t[0]
+            return t[0]
+        if inverse:
+            s += t[0] * (1 / t[1]) / weight_sum
+        else:
+            s += t[0] * t[1] / weight_sum
+    return s
+
+
+# Uses k - Nearest Neighbor technique to get the coordinates associated with
+# the given AccessPoint dictionary
+def apply_kNN(data, aps, k = 3):
+    k = min(k, len(data))
+    #data = sorted(data, key=lambda x: x.get_distance1(aps))
+    for d in data:
+        d.distance = d.get_distance1(aps)
+    data = sorted(data, key=lambda x: x.distance)
+    #TODO: Reconsider avg vs. mode
+    d = Counter([loc.floor_id for loc in data[:(k * 2 - 1)]])
+    floor = d.most_common(1)[0][0]
+    data = [d for d in data if d.floor_id == floor]
+    x = weighted_avg([(loc.x, loc.distance) for loc in data[:k]], True)
+    y = weighted_avg([(loc.y, loc.distance) for loc in data[:k]], True)
+    return (x, y, floor)
+
+
+# Returns the standard deviation of the given list
+def get_sd(l):
+    mean = get_mean(l)
+    rVal = 0
+    for elem in l:
+        rVal += (elem - mean) ** 2
+    return (rVal / (len(l) - 1)) ** .5
+
+# Returns the mean of the given list
+def get_mean(l):
+    return sum(l) / len(l)
+
+# Normalizes the signal strengths of all AccessPoints in the given array of
+# Locations and the given AccessPoint dictionary
+def normalize(data, aps):
+    global MIN_DETECTED
+    strengths = []
+    for loc in data:
+        for ap in loc.aps.values():
+            strengths.append(ap.strength)
+    mean = get_mean(strengths)
+    st_dev = get_sd(strengths)
+    for loc in data:
+        for ap in loc.aps.values():
+            ap.strength = (ap.strength - mean) / st_dev
+            if ap.strength < MIN_DETECTED:
+                MIN_DETECTED = ap.strength
+    for ap in aps.values():
+        ap.strength = (ap.strength - mean) / st_dev
+        if ap.strength < MIN_DETECTED:
+            MIN_DETECTED = ap.strength
+
+# Returns a list of Locations and an AccessPoint dictionary
+def get_locations(data):
+    locations = []
+    #sys.stderr.write("LENGTH: " + str(len(data)) + "\n")
+    for d in data:
+        cur_macs = d["macs"]
+        cur_rss = d["rss"]
+        cur_aps = []
+        for i in range(len(cur_macs)):
+            cur_aps.append((cur_macs[i], cur_rss[i], 0, 0))
+        locations.append((d["x"], d["y"], d["direction"], d["floor_id"], cur_aps))
+    return [Location(i) for i in locations]
+
+def getData(db_cursor=None):
+    if db_cursor is None:
+        from scripts.db.db import Database
+        password = os.environ.get('SIRIUS_PASSWORD')
+        if password is None:
+            raise Exception('No database password available')
+
+        db = Database(password)
+
+        cur = db.get_cur()
+    else:
+        cur = db_cursor
+    cur.execute("""SELECT floor_id,marauder_accesspoint.location_id, x_coordinate, y_coordinate, direction,
+         array_to_string(array_agg(mac_address),',') as MAC_list,
+         array_to_string(array_agg(signal_strength),',') as strength_list 
+         from marauder_accesspoint 
+         join marauder_location 
+            on marauder_location.id=marauder_accesspoint.location_id
+         group by floor_id,marauder_accesspoint.location_id,x_coordinate,y_coordinate,direction""")
+    access_points = cur.fetchall()
+    res = []
+    for f in access_points:
+        msg = {
+            'floor_id': f[0],
+            'location_id': f[1],
+            'x': f[2],
+            'y': f[3],
+            'direction': f[4],
+            'macs': f[5].split(','),
+            'rss': map(float, f[6].split(','))
+        }
+        res.append(msg)
+    return res
+
+def kNN(test_aps, db_cursor=None):
+    test_aps = {ap['mac_address'] : AccessPoint(ap, from_django=True) for ap in test_aps}
+    trained_data = getData(db_cursor=db_cursor)
+    locations = get_locations(trained_data)
+    normalize(trained_data, test_aps)
+    return apply_kNN(trained_data, test_aps)
